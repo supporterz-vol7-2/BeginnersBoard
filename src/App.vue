f751974eaf513a5f4e1ddbc7b659c1e11ba51d69
araitasuku <araiprog.11py@gmail.com>
2021-09-10 12:23:45 +0900

    add"|"

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="nav">
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link> |
-    <router-link to="/Create-Post">Create-Post</router-link>
+    <router-link to="/Create-Post">Create-Post</router-link>|
     <router-link to="/board">board</router-link>|
     <router-link to="/post">Post</router-link>
   </div>
